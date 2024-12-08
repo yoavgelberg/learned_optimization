123e87cfb2767b365d3db3ee6755762b43d9e3e6
Yoav Gelberg <yoavg98@gmail.com>
2024-12-08 10:06:08 +0200

    HookableMLP attempt

--- playground.py
+++ playground.py
@@ -3,49 +3,65 @@ import jax.numpy as jnp
 import haiku as hk
 import oryx
 
-
-def record_vjp(f, name):
-    @jax.custom_vjp
-    def custom_f(*args):
-        return f(*args)
-
-    def custom_fwd(*args, **kwargs):
-        return f(*args, **kwargs), args
-
-    def custom_bwd(res, dy):
-        _, vjp = jax.vjp(f, *res)
-        return oryx.core.sow(vjp(dy), tag="vjp", name=name)
-
-    custom_f.defvjp(custom_fwd, custom_bwd)
-    return custom_f
+from typing import Sequence, Callable
 
 
-class Model(hk.Module):
-    def __init__(self):
+class HookableMLP(hk.Module):
+    def __init__(
+        self,
+        hidden_dims: Sequence[int],
+        output_dim: int,
+        activation: Callable = jax.nn.relu,
+    ):
         super().__init__(name="model")
 
-    def __call__(self, x):
-        x = oryx.core.sow(
-            record_vjp(hk.Linear(10))(x, name="g_0"), tag="activations", name="a_0"
-        )
-        x = jax.nn.relu(x)
-        x = oryx.core.sow(
-            record_vjp(hk.Linear(10))(x, name="g_1"), tag="activations", name="a_1"
-        )
-        x = jax.nn.relu(x)
-        x = oryx.core.sow(
-            record_vjp(hk.Linear(1))(x, name="g_2"), tag="activations", name="a_2"
+        self.hidden_dims = hidden_dims
+        self.output_dim = output_dim
+        self.activation = activation
+
+    def __call__(self, x, *args):
+        x = oryx.core.sow(x, name="a_0", tag="activations") + args[0]
+
+        for i, hidden_dim in enumerate(self.hidden_dims):
+            x = (
+                oryx.core.sow(
+                    hk.Linear(hidden_dim)(x), tag="activations", name=f"a_{i + 1}"
+                )
+                + args[i + 1]
+            )
+            x = self.activation(x)
+
+        x = (
+            oryx.core.sow(
+                hk.Linear(self.output_dim)(x),
+                tag="activations",
+                name=f"a_{len(self.hidden_dims) + 1}",
+            )
+            + args[-1]
         )
         return x
 
 
-model = hk.without_apply_rng(hk.transform(lambda x, b1, b2: Model()(x, b1, b2)))
+model = hk.without_apply_rng(
+    hk.transform(
+        lambda x, b1, b2, b3, b4: HookableMLP(hidden_dims=[10, 10], output_dim=1)(
+            x, b1, b2, b3, b4
+        )
+    )
+)
 params = model.init(
-    jax.random.PRNGKey(0), jnp.ones([10]), jnp.zeros([10]), jnp.zeros([10])
+    jax.random.PRNGKey(0),
+    jnp.ones([10]),
+    jnp.zeros([10]),
+    jnp.zeros([10]),
+    jnp.zeros([10]),
+    jnp.zeros([1]),
 )
 
 
 x = jnp.ones([10])
 b1 = jnp.zeros([10])
 b2 = jnp.zeros([10])
-print(model.apply(params, x, b1, b2))
+b3 = jnp.zeros([10])
+b4 = jnp.zeros([1])
+print(oryx.core.trace(model.apply)(params, x, b1, b2, b3, b4))
